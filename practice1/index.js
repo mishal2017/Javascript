1386300ad7eb5537143152c342fe4840260db758
Rabeya Hayder Mishal <mishal7oct@gmail.com>
2023-04-15 10:07:23 +0600

    type of function practice

---
+++ practice1/index.js
@@ -0,0 +1,10 @@
+var num = 12.235;
+num=toString(num);
+console.log(typeof(num));
+//num=parseFloat(num);
+
+
+//console.log(num.toFixed(2));
+//console.log(num.toPrecision(3));
+//console.log(Number("2454"));
+//console.log(Number(true));
